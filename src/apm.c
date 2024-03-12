16d8a4405da1633c463824cdc9d20362d1b6e56a
NathanaeldeLaChesnais <nathanael.houitte-de-la-chesnais@polytechnique.edu>
2024-03-12 14:18:27 +0100

    suppression des warning pour %lld à la place de %ld

--- src/apm.c
+++ src/apm.c
@@ -45,7 +45,7 @@ char *read_input_file(char *filename, int *size) {
     /* Allocate data to copy the target text */
     buf = (char *)malloc(fsize * sizeof(char));
     if (buf == NULL) {
-        fprintf(stderr, "Unable to allocate %lld byte(s) for main array\n",
+        fprintf(stderr, "Unable to allocate %ld byte(s) for main array\n",
                 fsize);
         return NULL;
     }
@@ -54,7 +54,7 @@ char *read_input_file(char *filename, int *size) {
     if (n_bytes != fsize) {
         fprintf(
             stderr,
-            "Unable to copy %lld byte(s) from text file (%d byte(s) copied)\n",
+            "Unable to copy %ld byte(s) from text file (%d byte(s) copied)\n",
             fsize, n_bytes);
         return NULL;
     }
