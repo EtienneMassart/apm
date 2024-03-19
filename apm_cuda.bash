83f2d41cb61c51a2079fc166702bf0594bf44a10
NathanaeldeLaChesnais <nathanael.houitte-de-la-chesnais@polytechnique.edu>
2024-03-19 13:19:43 +0100

    last

--- apm_cuda.bash
+++ apm_cuda.bash
@@ -1,4 +1,5 @@
-./apm_cuda 0 dna/big_chr.fa ATTGCGTCAATTAGTCTAGTAC
+# ./apm_cuda 0 dna/big_chr.fa ATTGCGTCAATTAGTCTAGTAC
+./apm_cuda 0 dna/big_chr.fa AGAGACACCAGGCCCACGACAAACATAAGTTCTAGCCAAACGTGTTATAA
 
 
 # ça devient plus interressant de faire du CUDA à partir d'un patern de 20 nucléotides
